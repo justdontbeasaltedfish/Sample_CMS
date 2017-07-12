dafacbac4d10cb96e6a97c95e911a756d4fd9a86
WangJianrui <wangjianrui957@outlook.com>
2017-07-12 18:01:32 +0800

    Initial commit

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => '39.108.130.201',
+    'DB_USER' => 'WangJianrui',
+    'DB_PWD' => 'wangjianruiG0!',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'imooc_singcms',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' => 'cms_',
+);
